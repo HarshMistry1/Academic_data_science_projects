e156018808f5db60dfe3c67a3abf82e26bbca9fb
Harsh <Harshsutharinfo@gmail.com>
2022-02-09 22:59:28 +0530

    add chatbot

---
+++ chatbot/chatapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class chatdata(models.Model):
+    name=models.CharField(max_length=50,default=None)
+    msg=models.TextField(default=None)
+    reply=models.TextField(default=None)
+    
+    def __str__(self):
+        return self.name
+class chat(models.Model):
+    msg=models.CharField(max_length=200)
